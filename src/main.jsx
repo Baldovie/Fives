db3dfaedf69acaad255d884562f62a87647a6d0a
Baldovie <ryan.wallace@live.co.uk>
2025-01-26 20:13:24 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import './Sevens.css';
 import Sevens from './Sevens.jsx';
 import Number from './Number.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Number
-      id="numberTwo"
-      style="number-tile two"
-      numberStyle="number-value white"
-      value="2"
-    />
+    <Number id="numberTwo" style="number-tile two" value="2" />
     <h1>Fives!</h1>
     <Sevens />
   </StrictMode>
